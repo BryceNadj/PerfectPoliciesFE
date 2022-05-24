6e6621e183770bf413a83489ea88dd089bf95496
Bryce Nadj <bryce.nadj@gmail.com>
2022-05-24 15:21:11 +1000

    Add display names

--- PerfectPoliciesFE/Models/QuizModels/Quiz.cs
+++ PerfectPoliciesFE/Models/QuizModels/Quiz.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using PerfectPoliciesFE.Models.QuestionModels;
 
 namespace PerfectPoliciesFE.Models.QuizModels
@@ -7,13 +8,17 @@ namespace PerfectPoliciesFE.Models.QuizModels
     public class Quiz
     {
         // Primary Key
+        [Display(Name = "Quiz Id")]
         public int QuizId { get; set; }
 
         // Attributes
         public string Title { get; set; }
         public string Topic { get; set; }
         public string Author { get; set; }
+        
+        [Display(Name = "Date Created")]
         public DateTime? DateCreated { get; set; }
+        [Display(Name = "Passing Grade")]
         public int PassingGrade { get; set; }
 
         // Navigation Property
